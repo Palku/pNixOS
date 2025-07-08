5f88950ea87dd69d94c1b7dc81be3d6cc8635e5d
Johan Lindberg <johan.lindberg@dlaboratory.com>
2025-07-08 20:02:48 +0200

    more missing stuff

---
+++ modules/nixos/desktop.nix
@@ -0,0 +1,59 @@
+# modules/nixos/desktop.nix - Desktop environment
+{ config, pkgs, hyprland, ... }:
+
+{
+  # Hyprland
+  programs.hyprland = {
+    enable = true;
+    package = hyprland.packages."x86_64-linux".hyprland;
+  };
+
+  # XDG Portal
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-hyprland
+      xdg-desktop-portal-gtk
+    ];
+    config.common.default = "*";
+  };
+
+  # Audio
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  # Fonts
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      jetbrains-mono
+      font-awesome
+      (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" ]; })
+    ];
+    fontconfig = {
+      defaultFonts = {
+        serif = [ "Noto Serif" ];
+        sansSerif = [ "Noto Sans" ];
+        monospace = [ "JetBrains Mono" ];
+      };
+    };
+  };
+
+  # Polkit
+  security.polkit.enable = true;
+
+  # GNOME Keyring
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.login.enableGnomeKeyring = true;
+}
